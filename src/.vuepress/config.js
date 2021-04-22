35c1da95a82c0348b67075f5ebcf0e2816f811fb
Max Weisel <max@maxweisel.com>
2021-04-22 17:08:43 -0400

    Create test sidebar layout.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -33,95 +33,226 @@ module.exports = {
       {
         title: 'Home',
         path: '/',
-        collapsable: true,
+        collapsable: false,
         sidebarDepth: 1,
       },
       {
-        title: 'Getting Started',
-        path: '/getting-started',
-        collapsable: true,
-        sidebarDepth: 1,
-      },
-      {
-        title: "Core Concepts",
+        title: "Essentials",
         collapsable: false,
         sidebarDepth: 2,
         children: [
-          '/core-concepts/how-normcore-works',
-          '/core-concepts/synchronizing-custom-data',
-          '/core-concepts/server-authority-ownership-and-lifetime-flags',
-          '/core-concepts/networked-physics',
+          { title: 'What is Normcore?', path: '/test' },
+          { title: 'Getting started', path: '/test' },
+          { title: 'How Normcore works', path: '/test' },
+          { title: 'Common questions', path: '/test' },
         ]
+      },
+      {
+        title: "Pricing",
+        path: '/test',
+        collapsable: false,
+        sidebarDepth: 2,
       },
       {
         title: "Guides",
         collapsable: false,
         sidebarDepth: 2,
         children: [
-          '/guides/creating-a-player-controller',
-          //'/guides/creating-a-soccer-game',
-          '/guides/upgrading-from-normcore-1-to-normcore-2',
+          { title: 'Creating a player controller', path: '/test' },
+          { title: 'XR Avatars and Voice Chat', path: '/test' },
+          { title: 'Creating a multiplayer drawing app', path: '/test' },
+          { title: 'Using AR as a spectator view', path: '/test' },
+          { title: 'Upgrading from Normcore 1 to Normcore 2', path: '/test' },
         ]
       },
       {
-        title: "XR Guides",
+        title: "Architecture",
         collapsable: false,
-        sidebarDepth: 1,
+        sidebarDepth: 2,
         children: [
-          '/xr-guides/xr-avatars-and-voice-chat',
-          //'/xr-guides/teleporting',
-          //'/xr-guides/throwing-objects',
-          '/xr-guides/creating-a-multiplayer-drawing-app',
-          '/xr-guides/using-ar-as-a-spectator-view',
+          { title: 'Overview', path: '/test' },
+          { title: 'Client', path: '/test' },
+          { title: 'Server', path: '/test' },
+          { title: 'Transport', path: '/test' },
         ]
       },
       {
-        title: 'Reference',
-        path: '/reference/',
+        title: "Realtime API",
         collapsable: false,
-        sidebarDepth: 0,
+        sidebarDepth: 2,
         children: [
-          '/reference/realtime',
-          '/reference/realtimeavatar',
-          '/reference/realtimeavatarmanager',
-          '/reference/realtimeavatarvoice',
-          '/reference/realtimetransform',
-          '/reference/realtimeview',
-          '/reference/room',
-          '/reference/audiooutput',
-          '/reference/native-audiooutputstream',
-          '/reference/native-audioinputstream',
-          '/reference/serialization-realtimearray',
-          '/reference/serialization-realtimedictionary',
-          '/reference/serialization-realtimeset'
+          { title: 'Realtime', path: '/test' },
+          { title: 'RealtimeView', path: '/test' },
+          { title: 'RealtimeComponent', path: '/test' },
+          { title: 'RealtimeTransform', path: '/test' },
+          { title: 'Synchronizing Custom Data', path: '/test' },
+          { title: 'Networked Physics', path: '/test' },
+          { title: 'FAQ', path: '/test' },
         ]
       },
       {
-        title: "Normcore Private",
-        //path: '/normcore-private/',
+        title: "Room + Datastore API",
         collapsable: false,
-        sidebarDepth: 0,
+        sidebarDepth: 2,
         children: [
-          {
-            title: "Cloud",
-            collapsable: false,
-            sidebarDepth: 1,
-            children: [
-              '/normcore-private/cloud-architecture',
-              '/normcore-private/webhooks',
-              '/normcore-private/on-premises/google-cloud-installation',
-            ]
-          },
-          // {
-          //   title: "Standalone",
-          //   collapsable: false,
-          //   sidebarDepth: 1,
-          //   children: [
-          //     '/normcore-private/on-premises/standalone',
-          //   ]
-          // },
+          { title: 'Room', path: '/test' },
+          { title: 'Datastore', path: '/test' },
+          { title: 'RealtimeModel', path: '/test' },
+          { title: 'Collections', path: '/test' },
+          { title: 'Ownership + Lifetime Flags', path: '/test' },
+          { title: 'RPCs', path: '/test' },
+          { title: 'FAQ', path: '/test' },
         ]
       },
+      {
+        title: "Media API",
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          { title: 'Coming Soon', collapsable: false },
+        ]
+      },
+      {
+        title: "Servers",
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          { title: 'Architecture', path: '/test' },
+          { title: 'Matcher', path: '/test' },
+          { title: 'Clusters', path: '/test' },
+          { title: 'Rooms', path: '/test' },
+          { title: 'Authoritative Servers', path: '/test' },
+          { title: 'Normcore Private', path: '/test' },
+          { title: 'FAQ', path: '/test' },
+        ]
+      },
+      {
+        title: "Platforms",
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          { title: 'Native', path: '/test' },
+          { title: 'WebGL', path: '/test' },
+        ]
+      },
+      {
+        title: "XR",
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          { title: 'Creating a XR avatar with voice chat', path: '/test' },
+          { title: 'RealtimeAvatar + Manager', path: '/test' },
+          { title: 'RealtimeAvatarVoice', path: '/test' },
+        ]
+      },
+      {
+        title: "Reference",
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          { title: 'AudioSendStream', path: '/test' },
+          { title: 'AudioReceiveStream', path: '/test' },
+          { title: 'Datastore', path: '/test' },
+          { title: 'IRealtimeComponent', path: '/test' },
+          { title: 'Realtime', path: '/test' },
+          { title: 'RealtimeView', path: '/test' },
+          { title: 'RealtimeComponent', path: '/test' },
+          { title: 'RealtimeTransform', path: '/test' },
+          { title: 'Room', path: '/test' },
+          { title: 'Serialization.RealtimeArray', path: '/test' },
+          { title: 'Serialization.RealtimeDictionary', path: '/test' },
+          { title: 'Serialization.RealtimeModel', path: '/test' },
+          { title: 'Serialization.RealtimeSet', path: '/test' },
+          { title: 'Serialization.SerializationContext', path: '/test' },
+          { title: 'Serialization.StringKeyDictionary', path: '/test' },
+        ]
+      },
+      // {
+      //   title: 'Getting Started',
+      //   path: '/getting-started',
+      //   collapsable: true,
+      //   sidebarDepth: 1,
+      // },
+      // {
+      //   title: "Core Concepts",
+      //   collapsable: false,
+      //   sidebarDepth: 2,
+      //   children: [
+      //     '/core-concepts/how-normcore-works',
+      //     '/core-concepts/synchronizing-custom-data',
+      //     '/core-concepts/server-authority-ownership-and-lifetime-flags',
+      //     '/core-concepts/networked-physics',
+      //   ]
+      // },
+      // {
+      //   title: "Guides",
+      //   collapsable: false,
+      //   sidebarDepth: 2,
+      //   children: [
+      //     '/guides/creating-a-player-controller',
+      //     //'/guides/creating-a-soccer-game',
+      //     '/guides/upgrading-from-normcore-1-to-normcore-2',
+      //   ]
+      // },
+      // {
+      //   title: "XR Guides",
+      //   collapsable: false,
+      //   sidebarDepth: 1,
+      //   children: [
+      //     '/xr-guides/xr-avatars-and-voice-chat',
+      //     //'/xr-guides/teleporting',
+      //     //'/xr-guides/throwing-objects',
+      //     '/xr-guides/creating-a-multiplayer-drawing-app',
+      //     '/xr-guides/using-ar-as-a-spectator-view',
+      //   ]
+      // },
+      // {
+      //   title: 'Reference',
+      //   path: '/reference/',
+      //   collapsable: false,
+      //   sidebarDepth: 0,
+      //   children: [
+      //     '/reference/realtime',
+      //     '/reference/realtimeavatar',
+      //     '/reference/realtimeavatarmanager',
+      //     '/reference/realtimeavatarvoice',
+      //     '/reference/realtimetransform',
+      //     '/reference/realtimeview',
+      //     '/reference/room',
+      //     '/reference/audiooutput',
+      //     '/reference/native-audiooutputstream',
+      //     '/reference/native-audioinputstream',
+      //     '/reference/serialization-realtimearray',
+      //     '/reference/serialization-realtimedictionary',
+      //     '/reference/serialization-realtimeset'
+      //   ]
+      // },
+      // {
+      //   title: "Normcore Private",
+      //   //path: '/normcore-private/',
+      //   collapsable: false,
+      //   sidebarDepth: 0,
+      //   children: [
+      //     {
+      //       title: "Cloud",
+      //       collapsable: false,
+      //       sidebarDepth: 1,
+      //       children: [
+      //         '/normcore-private/cloud-architecture',
+      //         '/normcore-private/webhooks',
+      //         '/normcore-private/on-premises/google-cloud-installation',
+      //       ]
+      //     },
+      //     // {
+      //     //   title: "Standalone",
+      //     //   collapsable: false,
+      //     //   sidebarDepth: 1,
+      //     //   children: [
+      //     //     '/normcore-private/on-premises/standalone',
+      //     //   ]
+      //     // },
+      //   ]
+      // },
     ]
   }
 }
