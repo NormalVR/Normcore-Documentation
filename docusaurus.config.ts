3526dbb495beb3a4f2b05ff970b19e309f9d5b32
Max Weisel <max@maxweisel.com>
2024-12-30 14:19:34 -0500

    Fix base URL for docusaurus.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   tagline: 'Seamless multiplayer for Unity.',
 
   // Set the production url of your site here
-  url: 'https://normcore.io/',
+  url: 'https://docs.normcore.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
