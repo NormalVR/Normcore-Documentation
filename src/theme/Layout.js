a1a78e2f565b614f1003e846394bd44cbc083f8b
Max Weisel <max@maxweisel.com>
2025-02-04 14:35:07 -0500

    Add Mixpanel.

---
+++ src/theme/Layout.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import OriginalLayout from '@theme-original/Layout';
+import mixpanel from 'mixpanel-browser';
+
+export default function Layout(props) {
+  useEffect(() => {
+    mixpanel.init('f41d2eb4b3b4db270f606e87b754c06c', {
+      track_pageview: 'full-url',
+      persistence: 'localStorage',
+    });
+  }, []);
+
+  return <OriginalLayout {...props} />;
+}
