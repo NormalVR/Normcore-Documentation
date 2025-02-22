abc8c8aafb06e7fc168a432f9a6e259e8d453592
Max Weisel <max@maxweisel.com>
2025-02-22 00:23:17 -0500

    Revert updated download link because Docusaurus treats it as a broken link.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
         },
         {
           label: 'Download',
-          href: '/download',
+          href: '/essentials/getting-started#download-normcore',
           position: 'right',
           className: 'navbar__item navbar__link custom_nav_time download_button',
         },
